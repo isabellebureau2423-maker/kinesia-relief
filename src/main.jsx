e8d947c96d9f5dc5c837074a6c3790e9838a1f04
isabellebureau2423-maker <isa2423@gmail.com>
2026-04-26 00:20:04 +0200

    KinesiaRelief app Vite

---
+++ src/main.jsx
@@ -0,0 +1,1 @@
+import React from 'react'; import ReactDOM from 'react-dom/client'; import App from './KinesiaReliefV19.jsx'; ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>);
